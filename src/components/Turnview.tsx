4c0e5cf9e522d67048b32deace434dbd5a05d60a
antelor <ramiro.antelo4@gmail.com>
2024-05-05 22:47:09 -0300

    added movement to animation

--- src/components/Turnview.tsx
+++ src/components/Turnview.tsx
@@ -3,9 +3,11 @@ import '../styles/Turnview.scss'
 
 function Turnview({turn}: {turn: string}) {
     return (
-        <div className={`Turnview ${turn}`}>
-            Turn
-            <div className= 'turnSquare'></div>
+        <div className='Turnview'>
+            <div className={`turnSquare ${turn}`}>
+                Turn
+                <div className= 'turnSquareColor'></div>
+            </div>
         </div>
     )
 }
